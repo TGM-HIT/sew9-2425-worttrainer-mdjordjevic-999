932aebdebe0b3d8ad49a909225aed33dee420535
Marko Djordjevic <markodj015@gmail.com>
2024-10-16 13:23:31 +0200

    Die Methoden save und load in JSONSpellTrainerDAO mit JSON-Persistenzlogik implementiert.

--- src/main/java/com/example/spelltrainer/persistence/JSONSpellTrainerDAO.java
+++ src/main/java/com/example/spelltrainer/persistence/JSONSpellTrainerDAO.java
@@ -1,6 +1,18 @@
 package com.example.spelltrainer.persistence;
 
 import com.example.spelltrainer.model.SpellTrainer;
+import com.example.spelltrainer.model.WordImagePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Implementierung des SpellTrainerDAO mit JSON als Speicherformat.
@@ -14,15 +26,70 @@ public class JSONSpellTrainerDAO implements SpellTrainerDAO {
     }
 
     @Override
-    public void save(SpellTrainer spellTrainer) throws Exception {
-        // Vorbereitung für die JSON-Speicherung
-        // Hier wird später die Logik zur Speicherung implementiert
+    public void save(SpellTrainer spellTrainer) throws IOException, JSONException {
+        JSONObject json = new JSONObject();
+        JSONArray pairsArray = new JSONArray();
+
+        for (WordImagePair pair : spellTrainer.getWordImagePairs()) {
+            JSONObject pairJson = new JSONObject();
+            pairJson.put("word", pair.getWord());
+            pairJson.put("imageUrl", pair.getImageUrl().toString());
+            pairsArray.put(pairJson);
+        }
+
+        json.put("wordImagePairs", pairsArray);
+        json.put("totalAttempts", spellTrainer.getTotalAttempts());
+        json.put("correctAttempts", spellTrainer.getCorrectAttempts());
+        json.put("incorrectAttempts", spellTrainer.getIncorrectAttempts());
+
+        if (spellTrainer.getCurrentPair() != null) {
+            JSONObject currentPairJson = new JSONObject();
+            currentPairJson.put("word", spellTrainer.getCurrentPair().getWord());
+            currentPairJson.put("imageUrl", spellTrainer.getCurrentPair().getImageUrl().toString());
+            json.put("currentPair", currentPairJson);
+        }
+
+        try (FileWriter file = new FileWriter(filename)) {
+            file.write(json.toString(4));
+        }
     }
 
     @Override
-    public SpellTrainer load() throws Exception {
-        // Vorbereitung für das Laden von JSON
-        // Hier wird später die Logik zum Laden implementiert
-        return null;
+    public SpellTrainer load() throws IOException, MalformedURLException, JSONException {
+        File file = new File(filename);
+        if (!file.exists()) {
+            return null;
+        }
+
+        String content = new String(Files.readAllBytes(file.toPath()));
+        JSONObject json = new JSONObject(content);
+
+        JSONArray pairsArray = json.getJSONArray("wordImagePairs");
+        List<WordImagePair> wordImagePairs = new ArrayList<>();
+
+        for (int i = 0; i < pairsArray.length(); i++) {
+            JSONObject pairJson = pairsArray.getJSONObject(i);
+            WordImagePair pair = new WordImagePair(
+                    pairJson.getString("word"),
+                    pairJson.getString("imageUrl")
+            );
+            wordImagePairs.add(pair);
+        }
+
+        SpellTrainer spellTrainer = new SpellTrainer(wordImagePairs);
+        spellTrainer.setTotalAttempts(json.getInt("totalAttempts"));
+        spellTrainer.setCorrectAttempts(json.getInt("correctAttempts"));
+        spellTrainer.setIncorrectAttempts(json.getInt("incorrectAttempts"));
+
+        if (json.has("currentPair")) {
+            JSONObject currentPairJson = json.getJSONObject("currentPair");
+            WordImagePair currentPair = new WordImagePair(
+                    currentPairJson.getString("word"),
+                    currentPairJson.getString("imageUrl")
+            );
+            spellTrainer.setCurrentPair(currentPair);
+        }
+
+        return spellTrainer;
     }
 }

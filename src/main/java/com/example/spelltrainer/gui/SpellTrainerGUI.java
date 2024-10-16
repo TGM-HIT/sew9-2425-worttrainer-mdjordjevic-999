da3ef903f9c93ad065a40408c874ef7484172b81
Marko Djordjevic <markodj015@gmail.com>
2024-10-16 14:42:45 +0200

    Das gui-Package erstellt und die leere SpellTrainerGUI-Klasse hinzugefügt

---
+++ src/main/java/com/example/spelltrainer/gui/SpellTrainerGUI.java
@@ -0,0 +1,11 @@
+package com.example.spelltrainer.gui;
+
+/**
+ * Hauptklasse der grafischen Benutzeroberfläche für den Rechtschreibtrainer.
+ */
+public class SpellTrainerGUI {
+
+    public static void main(String[] args) {
+        // Startpunkt der Anwendung
+    }
+}

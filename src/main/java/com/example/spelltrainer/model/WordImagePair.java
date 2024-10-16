ada331707b37f34e6e3ee97773b697b403a0f1d5
Marko Djordjevic <markodj015@gmail.com>
2024-10-16 12:19:35 +0200

    Attribute word und imageUrl zu WordImagePair hinzugefügt und den Konstruktor implementiert.

--- src/main/java/com/example/spelltrainer/model/WordImagePair.java
+++ src/main/java/com/example/spelltrainer/model/WordImagePair.java
@@ -1,8 +1,17 @@
 package com.example.spelltrainer.model;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * Repräsentiert ein Paar aus Wort und zugehöriger Bild-URL.
  */
 public class WordImagePair {
-    // Leere Klasse
+    private String word;
+    private URL imageUrl;
+
+    public WordImagePair(String word, String imageUrl) throws MalformedURLException {
+        this.word = word;
+        this.imageUrl = new URL(imageUrl);
+    }
 }
